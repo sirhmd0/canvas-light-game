c3b365c13bc12a3e10dd12c2cc0512c2e543eeaa
Sir Hamid <64207154+sirhmd0@users.noreply.github.com>
2020-10-31 19:25:16 +0100

    Create canvas.js

---
+++ canvas.js
@@ -0,0 +1,5 @@
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const cw = canvas.width = window.innerWidth;
+const ch = canvas.height = window.innerHeight;
